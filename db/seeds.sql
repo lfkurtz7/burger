0fd2e9d49979de9dddf712a66b74ad1f2f1a648a
Leah Kurtz <lfkurtz7@gmail.com>
2019-12-02 18:57:21 -0500

    partials set up to make burger buttons work

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Trader Joes Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Whopper');
+INSERT INTO burgers (burger_name) VALUES ('In n Out');
---
+++ public/assets/app.js
@@ -0,0 +1,18 @@
+$(document).on("ready", function () {
+    // $("#submit").on("click", function (e) {
+    //     e.preventDefault();
+
+    //     var burger_name = $("input[name='name']").val();
+    //     var devoured = $("input[name='devoured']").prop("checked") ? 1 : 0
+    //     var data = {
+    //         burger_name,
+    //         devoured
+    //     }
+
+    //     $.post("/api/burgers", data)
+    //         .then(function (response) {
+    //             console.log(response)
+    //             location.reload()
+    //         })
+    // })
+})
