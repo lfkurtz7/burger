44ac63707613a5e753f8fc9a706ff259c5f3f648
Leah Kurtz <lfkurtz7@gmail.com>
2019-12-03 17:16:42 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+require("dotenv");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "dwa34you",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "dwa34you",
+    database: "burgers_db"
+  });
 
 // Make connection.
 connection.connect(function(err) {
@@ -17,6 +22,7 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+};
 
 // Export connection for our ORM to use.
 module.exports = connection;
